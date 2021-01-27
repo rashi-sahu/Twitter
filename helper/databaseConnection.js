afe92b497b960c30cf02da3dd41026778c9376d0
rashi-sahu <rashi.sahu13@gmail.com>
2021-01-27 20:47:04 +0530

    Setup dev and test databases

--- helper/databaseConnection.js
+++ helper/databaseConnection.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'twitter',
-  password: 'p',
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 });
 
 exports.connect = async () => {
---
+++ setup.js
@@ -0,0 +1,21 @@
+const clientConnector = require('./helper/databaseConnection');
+
+const testDatabaseName = 'twitter_test';
+const devDatabaseName = 'twitter_dev';
+
+const createDBIfNotExist = async (client, databaseName) => {
+  const result = await client.query('SELECT datname FROM pg_database;');
+  const findCheck = !result.rows.find((i) => i.datname === databaseName);
+  if (findCheck) {
+    await client.query(`CREATE DATABASE ${databaseName}`);
+    console.log(`successfully created ${databaseName}`);
+  }
+};
+
+clientConnector.connect().then(async (dbClient) => {
+  await createDBIfNotExist(dbClient, devDatabaseName);
+  await createDBIfNotExist(dbClient, testDatabaseName);
+  dbClient.end();
+}).catch((err) => {
+  console.log('error', err);
+});
