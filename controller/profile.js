927559cf2a0b0b5a569b5f9321f03b921bcca62d
rashi-sahu <rashi.sahu13@gmail.com>
2021-02-04 16:28:17 +0530

    Fetched tweets from database

--- controller/profile.js
+++ controller/profile.js
@@ -1,5 +1,18 @@
-exports.profile = (req, res) => {
+const userModel = require('../models/getTweets');
+
+const getTweets = async (dbClient, email) => {
+  const { error, result } = await userModel.getTweets(dbClient, email);
+  if (error) {
+    return 1;
+  }
+
+  return result.rows;
+};
+
+exports.profile = async (req, res) => {
   res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  const tweets = await getTweets(req.app.dbClient, req.params.username);
+  console.log('tweets', tweets);
   if (req.session.user && req.session.user.uname === req.params.username) {
     res.render('profile', { name: req.params.username });
   } else {
---
+++ models/getTweets.js
@@ -0,0 +1,16 @@
+module.exports.getTweets = async (client, email) => {
+  const select = `select t.description from tweets t 
+    inner join users u on u.id = t.createdby 
+    where u.emailid = '${email}';`;
+
+  let err = null;
+  let result = null;
+  try {
+    result = await client.query(select);
+    err = null;
+  } catch (e) {
+    err = e;
+    result = null;
+  }
+  return { error: err, result };
+};
