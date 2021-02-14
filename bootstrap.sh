0743d8adc1f5915c517e29a49c9cab1bf411d66c
rashi-sahu <rashi.sahu13@gmail.com>
2021-02-14 20:44:29 +0530

    Uncommented the bootstrap.sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,23 @@
-#!/usr/bin/env bash
-
-# apt-get update
-# sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-# curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-# sudo apt install nodejs -y
-# node -v
-# npm -v
+apt-get update
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs -y
+node -v
+npm -v
 cd /vagrant
-#  npm install
-# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+npm install
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
-# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
-# sudo apt-get update
-# sudo apt-get install -y postgresql-13
+sudo apt-get update
+sudo apt-get install -y postgresql-13
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
 export POSTGRES_USER=postgres
 export POSTGRES_HOST=localhost
 export POSTGRES_DATABASE=postgres
 export POSTGRES_PASSWORD=postgres
 export POSTGRES_PORT=5432
-echo "-----------------------------------------------------------"
-printenv
 npm run setup
 export POSTGRES_DATABASE=twitter_dev
 npm run tables
